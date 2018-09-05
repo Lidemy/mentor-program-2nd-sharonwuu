552215aa72c0d735126acb3472cf5b370bea4db4
sharonwuu <ojik565@gmail.com>
2018-09-06 01:53:23 +0800

    hw2_done

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,3 +1,16 @@
 function capitalize(str) {
-  
-}
+    let a=str.split('') 
+
+	if(a[0]!==a[0].toUpperCase()){
+    	a[0]=a[0].toUpperCase()
+    	return  a.join('')
+    }else{
+    	return str
+    }
+
+  }
+
+
+console.log(capitalize('nick'))
+console.log(capitalize('Nick'))
+console.log(capitalize(',hello'))

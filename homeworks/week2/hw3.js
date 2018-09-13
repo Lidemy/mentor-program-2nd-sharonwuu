1b52fb04a8e2b5b30061cbfa0561c4c74941665f
sharonwuu <ojik565@gmail.com>
2018-09-14 00:35:43 +0800

    hw3_done

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,18 @@
 function isPrime(n) {
-  
+  if( n===1){
+    return false
+  }else{
+  	for( let i=2 ; i<n ; i++){
+  		//console.log(`${n}除以${i}餘數是`,n%i)
+  	  if ( n%i === 0){
+  	    return false
+  	  }	
+	}
+  }
+  return true
 }
+
+
+
 
 module.exports = isPrime

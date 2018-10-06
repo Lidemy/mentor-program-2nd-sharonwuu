62d0f3064436e0245097fbeefe11b1183ed1abee
sharonwuu <ojik565@gmail.com>
2018-10-06 22:29:48 +0800

    all_done

---
+++ homeworks/week4/hw1/script.js
@@ -0,0 +1,61 @@
+let firstNum = '' 
+let secondNum = ''
+let noOperation = '' 
+	
+document.querySelector('.allBtns').addEventListener("click", function (e) {
+	let result = document.querySelector('.result') ;
+
+//console.log('target is :',e.target);
+//console.log('before Click noOperation is :',noOperation)
+//console.log('firstNum is : ',firstNum)
+
+	if( e.target.className === 'operation' ){
+		firstNum = result.innerText 
+		if(e.target.id === 'plus') {noOperation='+'}	
+		if(e.target.id === 'minus') {noOperation='-'}	
+		if(e.target.id === 'times') {noOperation='*'}	
+		if(e.target.id === 'obelus') {noOperation='/'}
+	}
+
+	if(e.target.id === 'equal'){
+		secondNum = result.innerText
+		if(noOperation==='+'){result.innerText =  parseFloat(Number(firstNum)+Number(secondNum))} 
+		if(noOperation==='-'){result.innerText =  parseFloat(Number(firstNum)-Number(secondNum))} 
+		if(noOperation==='*'){result.innerText =  parseFloat(Number(firstNum)*Number(secondNum))} 
+		if(noOperation==='/'){result.innerText =  parseFloat(Number(firstNum)/Number(secondNum))} 		
+	}
+
+	if (noOperation===''){ // noOperation => typing firstNum
+		if( e.target.className === 'num' ){
+			if( result.innerText === '0' ){
+				result.innerText = e.target.innerText
+			}else {
+				result.innerText += e.target.innerText
+			}
+		}
+		if( e.target.id === 'point' && !result.innerText.includes('.')){
+				result.innerText += e.target.innerText
+		}
+	}else if(noOperation!==''){ // !!!noOperation => typing secondNum
+		if( e.target.className === 'num' ){
+			if(result.innerHTML===firstNum){
+				result.innerText = e.target.innerText
+			}else{
+				result.innerText += e.target.innerText
+			}
+		}
+		if( e.target.id === 'point'){
+			if(result.innerHTML===firstNum){
+				result.innerText='0.'
+			}else if(!result.innerText.includes('.')){
+				result.innerText += e.target.innerText
+			}			
+		}
+	}					
+//console.log('=========================================')	
+	if(e.target.id === 'AC'){ 
+		window.location.reload();
+	} 
+		
+})
+
